eba644875678aff36c8fd6fada8bb76c5a53825d
James Colairo <jcolairo@spartaglobal.co>
2017-05-23 16:27:42 +0100

    make front and backend

---
+++ api/app.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var cors = require('cors');
+var logger = require('morgan');
+var bodyParser = require('body-parser');
+var app = express();
+
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/masters');
+
+var routes = require('./config/router');
+
+app.use(cors());
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(routes);
+
+app.listen(3000, function () {
+  console.log('listening on port 3000...');
+});
